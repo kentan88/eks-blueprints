43f8511c0580a581f7e92505a00490316973a56a
Ken Tan <ken.tan@thoughtworks.com>
2023-01-02 17:32:27 +0800

    second commit

--- examples/karpenter/main.tf
+++ examples/karpenter/main.tf
@@ -163,87 +163,20 @@ module "eks_blueprints_kubernetes_addons" {
   tags = local.tags
 }
 
-# Creates Launch templates for Karpenter
-# Launch template outputs will be used in Karpenter Provisioners yaml files. Checkout this examples/karpenter/provisioners/default_provisioner_with_launch_templates.yaml
-# module "karpenter_launch_templates" {
-#   source = "../../modules/launch-templates"
-
-#   eks_cluster_id = module.eks_blueprints.eks_cluster_id
-
-#   launch_template_config = {
-#     linux = {
-#       ami                    = data.aws_ami.eks.id
-#       launch_template_prefix = "karpenter"
-#       iam_instance_profile   = module.eks_blueprints.managed_node_group_iam_instance_profile_id[0]
-#       vpc_security_group_ids = [module.eks_blueprints.worker_node_security_group_id]
-#       block_device_mappings = [
-#         {
-#           device_name = "/dev/xvda"
-#           volume_type = "gp3"
-#           volume_size = 40
-#         }
-#       ]
-#     }
-#   }
-
-#   tags = merge(local.tags, { Name = "karpenter" })
-# }
-
 # Deploying default provisioner and default-lt (using launch template) for Karpenter autoscaler
-# data "kubectl_path_documents" "karpenter_provisioners" {
-#   pattern = "${path.module}/provisioners/default_provisioner*.yaml" # without launch template
-#   vars = {
-#     azs                     = join(",", local.azs)
-#     iam-instance-profile-id = "${local.name}-${local.node_group_name}"
-#     eks-cluster-id          = local.name
-#     eks-vpc_name            = local.name
-#   }
-# }
-
-# resource "kubectl_manifest" "karpenter_provisioner" {
-#   for_each  = toset(data.kubectl_path_documents.karpenter_provisioners.documents)
-#   yaml_body = each.value
-
-#   depends_on = [module.eks_blueprints_kubernetes_addons]
-# }
-
-resource "kubectl_manifest" "karpenter_provisioner" {
-  yaml_body = <<-YAML
-    apiVersion: karpenter.sh/v1alpha5
-    kind: Provisioner
-    metadata:
-      name: default
-    spec:
-      requirements:
-        - key: karpenter.sh/capacity-type
-          operator: In
-          values: ["spot"]
-      limits:
-        resources:
-          cpu: 1000
-      providerRef:
-        name: default
-      ttlSecondsAfterEmpty: 30
-  YAML
-
-  depends_on = [module.eks_blueprints_kubernetes_addons]
+data "kubectl_path_documents" "karpenter_provisioners" {
+  pattern = "${path.module}/provisioners/default_provisioner*.yaml" # without launch template
+  vars = {
+    azs                     = join(",", local.azs)
+    iam-instance-profile-id = "${local.name}-${local.node_group_name}"
+    eks-cluster-id          = local.name
+    eks-vpc_name            = local.name
+  }
 }
 
-resource "kubectl_manifest" "karpenter_node_template" {
-  yaml_body = <<-YAML
-    apiVersion: karpenter.k8s.aws/v1alpha1
-    kind: AWSNodeTemplate
-    metadata:
-      name: default
-    spec:
-      instanceProfile: "${local.name}-${local.node_group_name}"
-      subnetSelector:
-        karpenter.sh/discovery: "true"
-      securityGroupSelector:
-        karpenter.sh/discovery: ${local.name}
-      tags:
-        karpenter.sh/discovery: ${local.name}
-  YAML
+resource "kubectl_manifest" "karpenter_provisioner" {
+  for_each  = toset(data.kubectl_path_documents.karpenter_provisioners.documents)
+  yaml_body = each.value
 
   depends_on = [module.eks_blueprints_kubernetes_addons]
 }
